52e2ed36ba4e2265470f033560fe99a95f978920
Ansel Adams <ansel@MacMini.local>
2016-01-31 14:27:53 -0700

    first commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Ansel Adams on 1/31/16.
+//  Copyright © 2016 Ansel Adams. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
